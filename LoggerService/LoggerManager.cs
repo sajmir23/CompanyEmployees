4cbcbf7b214ce2fd9ebc8c2624ae3a11489d0e58
Ervis Lusha <=>
2024-02-13 12:49:38 +0100

    firstCommit

---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILoggerManager logger = LogManager.GetCurrentClassLogger();
+        public LoggerManager() 
+        {
+        }
+        public void LogDebug(string message) => logger.LogDebug(message);
+        public void LogError(string message) => logger.LogError(message);
+        public void LogInfo(string message) => logger.LogInfo(message);
+        public void LogWarn(string message) => logger.LogWarn(message);
+
+    }
+}
